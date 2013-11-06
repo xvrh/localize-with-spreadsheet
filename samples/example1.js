915ea01d70f0b06b0dc0c837cff960a4c4e51cfd
xavierha <xavier.hainaux@gmail.com>
2013-11-06 13:01:03 +0100

    Modify dart & json transformers

--- samples/example1.js
+++ samples/example1.js
@@ -3,8 +3,8 @@ var Localize = require("../index.js");
 var transformer = Localize.fromGoogleSpreadsheet("0Aq6WlQdq71FydDZlaWdmMEUtc2tUb1k2cHRBS2hzd2c");
 transformer.setKeyCol('KEY');
 
-transformer.save("samples/values/nl.dart", { valueCol: "NL", format: "dart", header: 'library pdwg.translations.nl;' });
-transformer.save("samples/values/fr.dart", { valueCol: "FR", format: "dart" });
+transformer.save("samples/values/nl.json", { valueCol: "NL", format: "json", header: '' });
+transformer.save("samples/values/fr.json", { valueCol: "FR", format: "json" });
 transformer.save("samples/values/r.dart", { valueCol: "FR", format: "dartTemplate", className: 'Translations' });
 transformer.save("samples/values/strings.xml", { valueCol: "NL", format: "android" });
 transformer.save("samples/values-fr/strings.xml", { valueCol: "FR", format: "android" });
